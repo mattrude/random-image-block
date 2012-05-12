0d67737d6f69a5464f5633e9e8ffdbb105536e3b
mattrude <m@mattrude.com>
2012-05-12 15:21:51 -0500

    update build page

--- build-page.sh
+++ build-page.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 PAGENAME="Random Image Block plugin for WordPress"
+README="readme.md"
 
-rm -f index.html README.md
+rm -f index.html $README
 cat header > index.html && \
-  git show master:README.md > README.md && \
-  sed -i "/# $PAGENAME/c <div id=\"title\"><h1>mattrude.github.com <i>&mdash; </i>$PAGENAME</h1></div><p><a href=\"/\">mattrude.github.com</a> / <strong>$PAGENAME</strong></p>" README.md && \
-  markdown README.md >> index.html && \
+  git show master:$README > $README && \
+  sed -i "/# $PAGENAME/c <div id=\"title\"><h1>mattrude.github.com <i>&mdash; </i>$PAGENAME</h1></div><p><a href=\"/\">mattrude.github.com</a> / <strong>$PAGENAME</strong></p>" $README && \
+  markdown $README >> index.html && \
   cat footer >> index.html
-rm -f README.md
+rm -f $README
